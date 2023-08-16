6ab08a3c87f2ba50cd5c2450d34fbf9db737be9a
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-08-16 00:15:11 -0700

    Implemented routes

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes/Project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+
+const Project = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Project;
