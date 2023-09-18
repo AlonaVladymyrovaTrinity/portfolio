0299b6d58603f4f07d92ae2e93d2c4643b9a1fbd
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-09-18 00:04:53 -0700

    Projects page implemented

---
+++ src/components/Projects/ProjectsList.js
@@ -0,0 +1,37 @@
+import AdoptTheDream from '../../assets/AdoptTheDream.png';
+import TodoApp from '../../assets/TodoApp.png';
+import BeatBazaar from '../../assets/TodoApp.png';
+import StarWars from '../../assets/StarWars.png';
+
+const ProjectsList = [
+  {
+    imgsrc: AdoptTheDream,
+    title: 'Adopt The Dream',
+    text: 'Team project "Adopt the Dream App" is a web application that connects pet enthusiasts with lovable animals in search of forever homes. This user-friendly application offers features such as registration, user profiles, pet browsing, favorites, advanced search, and adoption processing. Techniques used: Techniques used: React, React Router, Redux, Axios, React Auth Context, HTML5, CSS3, React bootstrap, JavaScript, Petfinder.com API, Stripe, Node.js, Express, JWT, MongoDB, socket.io, bcrypt, Swagger UI, Nodemailer, Jest',
+    view: 'https://adoptthedream.alona.dev',
+    source: 'https://github.com/AlonaVladymyrovaTrinity/AdoptTheDreamApp-Front',
+  },
+  {
+    imgsrc: BeatBazaar,
+    title: 'BeatBazaar',
+    text: 'Team project BeatBazaar represents a collaborative effort—a Music e-commerce app where users can seamlessly stream, curate wishlists, and purchase their favorite songs. Techniques used: React, React Router, Redux, Axios, React Auth Context, HTML5, CSS3, Material-ui, JavaScript, APIs, Stripe, Node.js, Express, JWT, MongoDB, socket.io, bcrypt, Swagger UI, Nodemailer, Jest',
+    view: 'https://beatbazaar.alona.dev',
+    source: 'https://github.com/AlonaVladymyrovaTrinity/',
+  },
+  {
+    imgsrc: TodoApp,
+    title: 'Todo App',
+    text: 'Personal project Todo App designed for effortless task management, this project streamlines user to-do list experience. It offers a user-friendly interface and features like creating custom to-do lists, easy editing and deletion, seamless sorting and searching, dark mode, an intuitive navigation menu, a dedicated notes section, and responsive design for all devices. Techniques used: React, React Router, HTML5, CSS3, JavaScript, APIs, Airtable',
+    view: 'https://todoapp.alona.dev/',
+    source: 'https://github.com/AlonaVladymyrovaTrinity/ToDoApp-Airtable',
+  },
+  {
+    imgsrc: StarWars,
+    title: 'Star Wars',
+    text: 'Personal Project: Star Wars App - Streamline access to Star Wars API (https://swapi.dev/) data, displaying key categories such as characters, planets, films, species, vehicles, and starships. Techniques used: HTML5, CSS3, JavaScript, APIs',
+    view: 'https://starwars.alona.dev/',
+    source: 'https://github.com/AlonaVladymyrovaTrinity/star-wars-api',
+  },
+];
+
+export default ProjectsList;
