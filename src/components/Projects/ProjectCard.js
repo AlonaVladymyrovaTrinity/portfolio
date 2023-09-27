3e62a36dee2aec3e2817cf57bd2c37dfb4646128
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-09-26 22:37:29 -0700

    Projects Page improved: material-ui added

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,48 +1,56 @@
-import style from './ProjectCard.module.css';
-// import NavigateButton from '../ReusableComponents/NavigateButton/NavigateButton';
-
+// import style from './ProjectCard.module.css';
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// import Link from '@mui/material/Link';
 const ProjectCard = (props) => {
   return (
-    <div className={style['project-card']}>
-      <img src={props.imgsrc} alt={props.title} />
-      <h2 className={style['project-title']}>{props.title}</h2>
-      <div className={style['pro-details']}>
-        <p>{props.text}</p>
-        <div className={style['pro-btns']}>
-          <a href={props.view} target="_blank" rel="noreferrer" className="btn">
-            View Project
-          </a>
-          <a
-            href={props.source}
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Github Source
-          </a>
-          {/* <NavigateButton
-            linkName={`/${props.view}`}
-            children={'View Project'}
-            className="btn"
-          />
-          <NavigateButton
-            linkName={`/${props.source}`}
-            children={'Github Source'}
-            className="btn"
-          /> */}
-          {/* <NavLink to={props.view} className="btn">
-            Page
-          </NavLink>
-          <NavLink to={props.source} className="btn">
-            Source
-          </NavLink> */}
-        </div>
-      </div>
-    </div>
+    <Card
+      sx={{
+        maxWidth: '25rem',
+        margin: '1rem auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        boxShadow: 15,
+        padding: '10 px',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <CardMedia
+        component="img"
+        alt="screen shot of project"
+        height="40%"
+        image={props.imgsrc}
+        title={props.title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.text}
+        </Typography>
+      </CardContent>
+      <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button href={props.view} target="_blank" rel="noreferrer" size="small">
+          View Project
+        </Button>
+        <Button
+          href={props.source}
+          target="_blank"
+          rel="noreferrer"
+          //   variant="contained"
+        >
+          Github Source
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
-
 export default ProjectCard;
