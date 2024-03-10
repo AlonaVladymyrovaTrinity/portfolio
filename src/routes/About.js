59c19890b7c03509ec9edc552c01f7894af8daf3
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2024-03-10 01:15:56 -0800

    Resume updated

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,7 +5,7 @@ import Hero from '../components/ReusableComponents/Hero/Hero';
 const About = () => {
   return (
     <>
-      <Hero title="ABOUT ME" caption="I am a Font-End Developer" />
+      <Hero title="ABOUT ME" caption="I am a Front-End Developer" />
       <AboutPage />
     </>
   );
